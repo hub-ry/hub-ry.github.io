8498548626155151372156eb164fd64c45054a36
hub-ry <rhubbart@purdue.edu>
2025-02-25 16:45:39 -0500

    Deploy Astro site

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/blog/Blog4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FBlog4.mdx&astroContentModuleFlag=true")]]);
+		
